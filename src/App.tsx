b7b2c6d9588d084ba1c8170318fb6e28df3d5726
Luca Luz <94574250+lucaLuz@users.noreply.github.com>
2023-11-09 14:43:33 -0300

    heatmap

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,144 @@
 import { ApexOptions } from 'apexcharts';
 import ReactApexChart from 'react-apexcharts';
-import data from './barras.json'
+import data from './heatmp.json'
 
-const App: React.FC = () => {
+function generateData(count: number, yrange: { min: any; max: any; }) {
+  var i = 0;
+  var series = [];
+  while (i < count) {
+    var x = (i + 1).toString();
+    var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+    series.push({ x: x, y: y });
+    i++;
+  }
+  return series;
+}
+
+const App = () => {
 
   const options: ApexOptions = {
-      chart: {
-        type: 'bar',
-        height: 350,
-        animations: {
-          enabled: false,
-        }
-      },
-
-      dataLabels: {
+    chart: {
+      height: 350,
+      type: 'heatmap',
+      animations:{
         enabled: false
-      },
-      stroke: {
-        show: true,
-        width: 2,
-        colors: ['transparent']
-      },
-      xaxis: {
-        categories:Object.values(data.VARIAVEL),
-      },
-      yaxis: {
-        title: {
-          text: '$ (thousands)'
-        },
-        labels:{
-          formatter: function(val){
-            return val.toFixed(1)
-          }
+      }
+    },
+    plotOptions: {
+      heatmap: {
+        shadeIntensity: 0.5,
+        radius: 0,
+        useFillColorAsStroke: true,
+        colorScale: {
+          ranges: [{
+              from: -30,
+              to: 5,
+              name: 'low',
+              color: '#00A100'
+            },
+            {
+              from: 6,
+              to: 20,
+              name: 'medium',
+              color: '#128FD9'
+            },
+            {
+              from: 21,
+              to: 45,
+              name: 'high',
+              color: '#FFB200'
+            },
+            {
+              from: 46,
+              to: 55,
+              name: 'extreme',
+              color: '#FF0000'
+            }
+          ]
         }
-      },
-      fill: {
-        opacity: 1
-      },
-    };
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      width: 1
+    },
+    title: {
+      text: 'HeatMap Chart with Color Range'
+    },
+  };
 
  const series:ApexAxisChartSeries = [{
-    name: Object.values(data.DESC).toString(),
-    data:Object.values(data['%'])
-  }];
+    name: 'Jan',
+    data: generateData(20, {
+      min: -30,
+      max: 55
+    })
+  },
+  {
+    name: 'Feb',
+    data: generateData(20, {
+      min: -30,
+      max: 55
+    })
+  },
+  {
+    name: 'Mar',
+    data: generateData(20, {
+      min: -30,
+      max: 55
+    })
+  },
+  {
+    name: 'Apr',
+    data: generateData(20, {
+      min: -30,
+      max: 55
+    })
+  },
+  {
+    name: 'May',
+    data: generateData(20, {
+      min: -30,
+      max: 55
+    })
+  },
+  {
+    name: 'Jun',
+    data: generateData(20, {
+      min: -30,
+      max: 55
+    })
+  },
+  {
+    name: 'Jul',
+    data: generateData(20, {
+      min: -30,
+      max: 55
+    })
+  },
+  {
+    name: 'Aug',
+    data: generateData(20, {
+      min: -30,
+      max: 55
+    })
+  },
+  {
+    name: 'Sep',
+    data: generateData(20, {
+      min: -30,
+      max: 55
+    })
+  }
+]
 
   return (
-    <>
-      <ReactApexChart options={options} series={series} type="bar" height={350} />
+<>
+      <ReactApexChart options={options} series={series} type="heatmap" height={350} />
     </>
   );
-};
+  }
 
 export default App;
